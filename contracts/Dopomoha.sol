6d834df1b3aba44c0e6332ceeb4610f4805f1608
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-05-08 23:28:34 +0200

    Some start contracts to test on tally

--- contracts/Dopomoha.sol
+++ contracts/Dopomoha.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract DopomohaToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("DopomohaToken", "DPMH") ERC20Permit("DopomohaToken") {
+contract Dopomoha is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("Dopomoha", "DPMH") ERC20Permit("Dopomoha") {
         _mint(msg.sender, 10 ** 24);
     }
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 
 async function main() {
 
-  const Contract = await ethers.getContractFactory("DopomohaToken");
+  const Contract = await ethers.getContractFactory("Dopomoha");
   const contract = await Contract.deploy();
 
   await contract.deployed();
